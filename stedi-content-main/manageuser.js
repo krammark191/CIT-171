09a8ad9fc34313e8b14ecedcc9f26329118712a1
Mark Van Horn <krammark191@gmail.com>
2022-05-05 14:32:12 -0600

    fixed an error

--- stedi-content-main/manageuser.js
+++ stedi-content-main/manageuser.js
@@ -51,7 +51,7 @@ function userlogin(){
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https//dev.stedi.me/login',
+        url: 'https://dev.stedi.me/login',
         data: JSON.stringify({userName, password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
