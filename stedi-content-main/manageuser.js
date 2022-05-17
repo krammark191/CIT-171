999f8fb536a8922ace7afdf28edb34871f3b8ef5
Mark Van Horn <krammark191@gmail.com>
2022-05-17 14:55:20 -0600

    Added OTP function

--- stedi-content-main/manageuser.js
+++ stedi-content-main/manageuser.js
@@ -1,6 +1,6 @@
 //© 2021 Sean Murdock
 
-let phonenumber = "";
+let phoneNumber = "";
 let userName = "";
 let password = "";
 let verifypassword = "";
@@ -11,16 +11,17 @@ let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
 // }
 
 function setphonenumber() {
-    phonenumber = $("#phonenumber").val();
+    phoneNumber = $("#phonenumber").val();
 
 }
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
+
+    // var valid=passwordRegEx.exec(password);
+    // if (!valid){
+    //     alert('Must be 6 digits, upper, lower, number, and symbol');
+    // }
 }
 
 function setverifypassword(){
@@ -55,7 +56,7 @@ function checkexpiredtoken(token){
 function sendtext() {
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/' + phonenumber,
+        url: 'https://dev.stedi.me/twofactorlogin/' + phoneNumber,
         contentType: "application/text",
         dataType: 'text'
     })
@@ -63,11 +64,10 @@ function sendtext() {
 
 function userlogin(){
     setuserpassword();
-    // setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber, oneTimePassword:password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
